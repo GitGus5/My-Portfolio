1c2f07e1e17eea433aad63ab5a038f8e33deae67
GitGus5 <93877136+GitGus5@users.noreply.github.com>
2023-11-06 21:47:21 -0600

    Add files via upload

--- Morris.info/JS/script.js
+++ Morris.info/JS/script.js
@@ -1,61 +1,49 @@
 // setTimeout(function(){
 //     location.reload();
 // }, 3000);
-
+const cl = console.log;
+// data for foreach statement to dynamiclly add skills
 const imageData = [
-  {
-      "name": "HTML5", 
-      "image": "Images/html-5.png"
-  },
-  {
-      "name": "CSS3", 
-      "image": "Images/css.png"
-  },
-  {
-      "name": "Javascript", 
-      "image": "Images/js.png"
-  },
-  {
-      "name": "JSON", 
-      "image": "Images/json-file.png"
-  },
-  {
-      "name": "GraphQL", 
-      "image": "Images/graphql.png"
-  },
-  {
-      "name": "Agile", 
-      "image": "Images/agile.png"
-  }
+    {
+        "name": "HTML5",
+        "image": "Images/html-5.png"
+    },
+    {
+        "name": "CSS3",
+        "image": "Images/css.png"
+    },
+    {
+        "name": "Javascript",
+        "image": "Images/js.png"
+    },
+    {
+        "name": "JSON",
+        "image": "Images/json-file.png"
+    },
+    {
+        "name": "GraphQL",
+        "image": "Images/graphql.png"
+    },
+    {
+        "name": "Agile",
+        "image": "Images/agile.png"
+    }
 ];
+var iconContainer = document.querySelector('.icon-wrapper');
 
+const imageString = [];
 
-let string = JSON.stringify(imageData);
-let parsedString = JSON.parse(string)
+function displayImageDataArray(object) {
+    // loops through each object in the array
+    object.forEach(objects => {
+        // .image gets image and .name gets the name
+        let html = '<div class="icon-word-container">' +
+            '<img src="' + objects.image + '" class="skills-images" id="" alt="' + objects.name + '">' +
+            '<p>' + objects.name + '</p>' +
+            '</div>';
 
-console.log();
-for (let index = 0; index < array.length; index++) {
-  const element = array[index];
-  parsedString["image", [i]]
+        iconContainer.innerHTML += html;
+    });
 }
 
-var object = {};
-var count = '';
-var querySelector = document.querySelector;
-querySelector = ['qs', 'QS', 'Qs', 'qS'];
-
-function populateSkillsImages() {
-  count = 0;
-  var html = qs(".icon-wrapper").innerHTML;
-  var imageParse = parsedString.image;
-  var valueImage = imageParse.value;
-  for (let i = 0; i < imageParse.length; i++) {
-    var element = imageParse[i];
-    
-    html += '<div class="icon-word-container">' +
-    '<!-- https://www.flaticon.com/free-icon/html-5_919827 -->' +
-    '<img src=" '+ imageParse[i] + ' " id="" alt="html" width="155" height="155">' +
-    '<p>HTML5</p>' +
-    '</div>';
-  };
-}
+displayImageDataArray(imageData);
