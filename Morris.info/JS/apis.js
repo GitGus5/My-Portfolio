5165378e3f6005bb8340c90b7fb519946853be43
GitGus5 <93877136+GitGus5@users.noreply.github.com>
2024-01-31 23:32:59 -0600

    Update apis.js

--- Morris.info/JS/apis.js
+++ Morris.info/JS/apis.js
@@ -1,24 +1,44 @@
+const delay = 500;
+
+// network request and delay
+const request = (url) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (delay <= 5000) {
+        fetch(url)
+          .then(response => response.json())
+          .then(data => resolve(data))
+          .catch(error => reject(error));
+      } else {
+        reject('Connection Timeout :(');
+      }
+    }, delay);
+  });
+};
+
+// Asynchronous function to get cat facts
 async function getCatFacts() {
+  try {
+    const catData = await request('https://catfact.ninja/facts');
+    let factsHtml = '';
 
-  const response = await fetch('https://catfact.ninja/facts');
-  const data = await response.json();
+    //iterate through 5 cat facts
+    for (let i = 0; i < 5; i++) {
+      // Get a random fact from data
+      const factIndex = Math.floor(Math.random() * catData.data.length);
+      const catFact = catData.data[factIndex];
 
-  let factsHtml = '';
+      // use factsHtml empty string to create muliple unique cat facts in HTML
+      factsHtml += `<p class="banner-box">😺 ${catFact.fact} 😺</p>`;
+    }
 
-  for (let i = 0; i < 5; i++) {
-    let count = 0;
-    // get a random fact
-    const factIndex = Math.floor(Math.random() * data.data.length);
-    const fact = data.data[factIndex];
-
-    factsHtml += `
-      <p class="banner-box">😺 ${fact.fact} 😺 </p>
-    `;
-
+    // The catFact data goes to HTML class .cat-information from factsHtml
+    document.querySelector(".cat-information").innerHTML = factsHtml;
   }
-
-  document.querySelector(".cat-information").innerHTML = factsHtml;
-
+  // catches any errors that occur
+  catch (error) {
+    console.error('Failed to load cat facts:', error);
+  }
 }
 
 getCatFacts();
